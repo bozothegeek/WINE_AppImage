97891e5a4a3e3086c0437152a80688e333484790
BOZO THE GEEK <bozolesv2@gmail.com>
2023-08-24 21:43:38 +0200

    add debug/verbose mode in shell script

--- build-custom-staging.sh
+++ build-custom-staging.sh
@@ -1,4 +1,8 @@
-#!/bin/bash -x
+#!/bin/bash
+#Display commands and their arguments as they are executed.
+set -x
+#Display shell input lines as they are read.
+set -v
 wget -q 'https://github.com/AppImageCrafters/appimage-builder/releases/download/v1.0.3/appimage-builder-1.0.3-x86_64.AppImage'
 chmod +x 'appimage-builder-1.0.3-x86_64.AppImage'
 ./appimage-builder-1.0.3-x86_64.AppImage --appimage-extract
