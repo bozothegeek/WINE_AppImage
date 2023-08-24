ee1c6414c898bf9983a524d35963e0aecc76e557
Bozo the geek <bozolesv2@gmail.com>
2023-08-24 19:31:29 +0200

    fix to use wine-staging-custom.yml

--- build-custom-staging.sh
+++ build-custom-staging.sh
@@ -4,5 +4,5 @@ chmod +x 'appimage-builder-1.0.3-x86_64.AppImage'
 ./appimage-builder-1.0.3-x86_64.AppImage --appimage-extract
 cp runtime/mksquashfs squashfs-root/usr/bin/mksquashfs
 sed -i 's|xz|zstd|' squashfs-root/usr/lib/python3.8/site-packages/appimagebuilder/modules/prime/appimage_primer.py
-squashfs-root/AppRun --recipe wine-stable.yml
+squashfs-root/AppRun --recipe wine-staging-custom.yml
 
