212d12848b3daa872d3232abc293863418dfa9c8
Bozo the geek <bozolesv2@gmail.com>
2023-08-24 19:23:39 +0200

    add build-custom-staging.sh to simplify tests of build

---
+++ build-custom-staging.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -x
+wget -q 'https://github.com/AppImageCrafters/appimage-builder/releases/download/v1.0.3/appimage-builder-1.0.3-x86_64.AppImage'
+chmod +x 'appimage-builder-1.0.3-x86_64.AppImage'
+./appimage-builder-1.0.3-x86_64.AppImage --appimage-extract
+cp runtime/mksquashfs squashfs-root/usr/bin/mksquashfs
+sed -i 's|xz|zstd|' squashfs-root/usr/lib/python3.8/site-packages/appimagebuilder/modules/prime/appimage_primer.py
+squashfs-root/AppRun --recipe wine-stable.yml
+
