fbc4050010195fb10bb55f2e56d560b8dce5dc9d
BOZO THE GEEK <bozolesv2@gmail.com>
2023-08-25 17:13:11 +0200

    add sh and yml files to build custom stable version

---
+++ build-custom-stable.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Display commands and their arguments as they are executed.
+set -x
+#Display shell input lines as they are read.
+set -v
+wget -q 'https://github.com/AppImageCrafters/appimage-builder/releases/download/v1.0.3/appimage-builder-1.0.3-x86_64.AppImage'
+chmod +x 'appimage-builder-1.0.3-x86_64.AppImage'
+./appimage-builder-1.0.3-x86_64.AppImage --appimage-extract
+cp runtime/mksquashfs squashfs-root/usr/bin/mksquashfs
+sed -i 's|xz|zstd|' squashfs-root/usr/lib/python3.8/site-packages/appimagebuilder/modules/prime/appimage_primer.py
+squashfs-root/AppRun --recipe wine-stable-custom.yml
+mv *.AppImage /media/sf_OUTPUT_BUILD/
+mv *.AppImage.zsync /media/sf_OUTPUT_BUILD/
+
